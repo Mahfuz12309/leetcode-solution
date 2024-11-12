ed0e3978a55cdfe51f3702aa0b76ce55cde9fa2e
Mahfuz12309 <mahfuz15-2713@diu.edu.bd>
2024-11-12 15:38:29 +0600

    changed

--- 26. Remove Duplicates from Sorted Array/code.java
+++ 26. Remove Duplicates from Sorted Array/code.java
@@ -1,8 +1,28 @@
 
 public class code {
-    public static void main(String[] args) {
-        int num[]={1,1,2};
-        
+    public static int removeDuplicates(int[] nums) {
+            if (nums == null || nums.length == 0) {
+                return 0; 
+            }
+            int i = 0;
+            for (int j = 1; j < nums.length; j++) {
+                if (nums[j] != nums[i]) {
+                    i++;  
+                    nums[i] = nums[j];  
+                }
+            }
+            
+            return i + 1;
+        }
+                
+    
+        public static void main(String[] args) {
+            int num[]={1,1,2};
+            int length=removeDuplicates(num);
+            for(int i=0;i<length;i++)
+            {
+                System.out.println(num[i]);
+            }
     }
     
 }
